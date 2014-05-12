a84a3f47be7f6853bca34e9196b7300c974134ad
bacondrake <frank.pavey@gmail.com>
2014-05-12 16:34:45 +1000

    Heroku updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,12 @@ Rails.application.configure do
 
   # Enable image storage in Heroku (Heroku does not keep images automatically) using aws-sdk (https://devcenter.heroku.com/articles/paperclip-s3)
   # This will not work because I do not have an account with aws
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+  #   config.paperclip_defaults = {
+  #     :storage => :s3,
+  #     :s3_credentials => {
+  #       :bucket => ENV['S3_BUCKET_NAME'],
+  #       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
